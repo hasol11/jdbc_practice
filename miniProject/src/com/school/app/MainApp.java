2c16b246e087c1a7a67eaf4d1bb977597f6e5da2
Jiyun Kang <155218786+hasol11@users.noreply.github.com>
2025-03-04 11:52:02 +0900

    Add files via upload

---
+++ miniProject/src/com/school/app/MainApp.java
@@ -0,0 +1,10 @@
+package com.school.app;
+
+import com.school.controller.SchoolController;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		SchoolController.startSchoolSystem();
+	}
+}
